ac9341c83909678b7b1e969fb1e0826d28148fee
Graham Conzett <conzett@gmail.com>
2012-09-12 16:13:54 -0400

    Add options for choosing display property
    The name of the option that will be used for the header display.

--- views/grid_view.js
+++ views/grid_view.js
@@ -1,9 +1,12 @@
 GridView = Backbone.View.extend({
+
     tagName : 'div',
     options : {
         columns : null,
-        columnMenu : false
+        columnMenu : false,
+        displayPropertyName : 'name'
     },
+
     initialize : function () {
         'use strict';
         this.render();
@@ -16,22 +19,25 @@ GridView = Backbone.View.extend({
             }
         });
     },
+
     render : function () {
         'use strict';
         $(this.el).html('<table><thead><tr></tr></thead><tbody></tbody></table><div id="show-hide-columns"></div>');
-        this.addHeader();
-        this.addRows();
+        this.renderHeader();
+        this.renderRows();
         if(!this.options.columnMenu) {
             $('#show-hide-columns', this.el).css('display', 'none');
         };
         return this;
     },
-    addHeader : function () {
+
+    renderHeader : function () {
         'use strict';
         _.each(this.options.columns, function (header) {
             var view = new GridHeaderCellView({
                 model : header,
-                grid : this
+                grid : this,
+                displayNameProperty : this.options.displayPropertyName
             });
             $('thead tr', this.el).append(view.el);
             var menu = new GridColumnMenuView({
@@ -41,7 +47,13 @@ GridView = Backbone.View.extend({
             $('#show-hide-columns', this.el).append(menu.el);
         }, this);
     },
-    addRow : function (row) {
+
+    renderRows : function () {
+        'use strict';
+        this.collection.each(this.renderRow, this);
+    },
+
+    renderRow : function (row) {
         'use strict';
         var view = new GridRowView({
             model : row,
@@ -49,10 +61,7 @@ GridView = Backbone.View.extend({
         });
         $('tbody', this.el).append(view.el);
     },
-    addRows : function () {
-        'use strict';
-        this.collection.each(this.addRow, this);
-    },
+
     sort : function (criteria, sortOrder) {
         'use strict';
         this.collection.comparator = function (column) {
@@ -69,14 +78,17 @@ GridView = Backbone.View.extend({
         this.collection.sort();
         this.render();
     },
+
     showColumnMenu : function () {
         $('#show-hide-columns', this.el).show();
         this.options.columnMenu = true;
     },
+
     hideColumnMenu : function () {
         $('#show-hide-columns', this.el).hide();
         this.options.columnMenu = false;
     },
+
     showColumn : function (column_name) {
         _.each(this.options.columns, function (header, index) {
             if (header.name === column_name) {
@@ -86,6 +98,7 @@ GridView = Backbone.View.extend({
         }, this);
         this.render();
     },
+
     hideColumn : function (column_name) {
         _.each(this.options.columns, function (header, index) {
             if (header.name === column_name) {
@@ -94,6 +107,7 @@ GridView = Backbone.View.extend({
             }
         }, this);
     },
+
     filterColumn : function (column_name, comparison) {
         var test = this.collection.filter(function (column) {
             return comparison.call(this, column.get(column_name));
