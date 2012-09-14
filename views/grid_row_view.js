40f864d5e502a806fa66cfc89f06aa85cf608ce7
Graham Conzett <conzett@gmail.com>
2012-09-14 13:16:51 -0400

    Use new column properties
    Use the new column properties to display the value.

--- views/grid_row_view.js
+++ views/grid_row_view.js
@@ -1,35 +1,37 @@
 window.GridRowView = Backbone.View.extend({
 
-    tagName : 'tr',
+  tagName : 'tr',
 
-    options : {
-        columns : null,
-        modelName : 'statistics',
-        columnID : 'attribute.id'
-    },
+  options : {
+    columns : null,
+    modelName : 'statistics',
+    columnID : 'attribute.id'
+  },
 
-    initialize : function () {
-        'use strict';
-        this.render();
-    },
+  initialize : function () {
+    'use strict';
+    this.render();
+  },
 
-    render : function () {
-        'use strict';
-        if (this.options.columns) {
-            _.each(this.options.columns, function (column) {
-                var cell = '<td';
-                if (column.hidden) {
-                    cell += ' class="hidden"'
-                }
-                cell += '>';
-
-               var value = column.valueFunction.apply(this, [this.model, column]);
-
-                cell += value + '</td>';
-
-                $(this.el).append(cell);
-            }, this);
+  render : function () {
+    'use strict';
+    if (this.options.columns) {
+      _.each(this.options.columns, function (column) {
+        var cell = '<td';
+        if (column.hidden) {
+          cell += ' class="hidden"'
         }
-        return this;
+        cell += '>';
+
+        // Find the object
+        var obj = column.cellObjectFunction.apply(this, [this.model, column]);
+        var value = typeof(obj) === 'undefined' ? '' : obj[column.cellObjectProperty]
+
+        cell += value + '</td>';
+
+        $(this.el).append(cell);
+      }, this);
     }
+    return this;
+  }
 });
